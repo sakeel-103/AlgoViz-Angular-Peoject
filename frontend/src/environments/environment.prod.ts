5aaa8cf11cb51d13bbfa8ed8729a4232d5209726
Md Sakeel Ansari <mdsakeel.sadabnoor786@gmail.com>
2025-06-15 12:27:20 +0530

    AlgoViz-Project

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// production environment
+export const environment = {
+  production: true,
+  BACKEND_API_URL: 'https://dfs-bfs-graph-travers.netlify.app/api',
+  YOUTUBE_API_KEY: 'AIzaSyBmTuFIkCRGP8K157l9RaijcLT7t7Wy1dM',
+  CLIENT_ID: 'test',
+};
