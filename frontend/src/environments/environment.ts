81bf8f5765029747289f47cdc1ccc749ad2e1df2
Md Sakeel Ansari <mdsakeel.sadabnoor786@gmail.com>
2025-06-16 14:07:24 +0530

    change

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  BACKEND_API_URL: 'algo-viz-angular-peoject.vercel.app/api',
+  BACKEND_API_URL: 'https://algo-viz-angular-peoject.vercel.app/api',
   YOUTUBE_API_KEY: 'AIzaSyBBnYSy5EKllIfdYEcL7WPdJuXkmCf7iCQ',
   CLIENT_ID: 'test',
 };
